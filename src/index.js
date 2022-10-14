30527186953282f4acb41cbe100677024d6c8ffc
Zoltan Mics <zoltanmics@gmail.com>
2022-10-15 00:35:57 +0200

    added 9.alleluja

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Tonus from './tonus';
 import ColorProvider from './colorProvider';
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let colorProvider = new  ColorProvider();
 const tonusok = numbers.map((number) => <Tonus counter={number} colourClass={colorProvider.getColour()}/>);
 
